290838a58ca79698d21d445b3e90a8f0b17dcc95
Allan Davis <cajun.code@gmail.com>
2023-12-13 21:09:49 -0600

    03 working process

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,10 +1,76 @@
 advent_of_code::solution!(3);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+use lazy_static::lazy_static;
+use regex::Regex;
+
+lazy_static!{
+    static ref NUMBER_REGEX: Regex = Regex::new("[0-9]{1,3}").unwrap();
+    static ref SYMBLE_REGEX: Regex = Regex::new(r"[\?|#|\$|\*|@|%|!|\^|\+|=]").unwrap();
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut lines = input.lines();
+    let mut previous: Option<&str> = None;
+    let mut current: Option<&str> = None;
+    let mut forward: Option<&str> = None;
+    let mut total: u32 = 0;
+    current = lines.next();
+    for line in lines{
+        total += check_for_parts(previous, current, Some(line));
+        previous = current;
+        current = Some(line);
+    }
+    total += check_for_parts(previous, current, forward);
+    Some(total)
+}
+
+fn check_for_parts(previous: Option<&str>, current: Option<&str>, forward:Option<&str>) -> u32 {
+    let mut total:u32 = 0;
+    for cap in NUMBER_REGEX.captures_iter(current.unwrap()){
+        let part = cap.get(0).unwrap().as_str();
+        let v:Vec<_> = current.unwrap().match_indices(part).collect();
+        let mut pos = v.first().unwrap().0;
+        if pos <= 0 {
+             pos = 0;
+        }else{
+            pos -=1;
+        }
+        let mut len = v.first().unwrap().1.len()+2+pos;
+
+        if len >= current.unwrap().len(){
+            len = current.unwrap().len();
+        }
+        let mut is_previous  = false;
+        let mut is_current = false;
+        let mut is_future = false;
+        if previous.is_some(){
+            let slice = &previous.unwrap()[pos .. len];
+            println!("cutting string {} at {pos}, {len}", previous.unwrap());
+            is_previous = SYMBLE_REGEX.is_match(slice.replace(".", " ").as_str());
+            println!("Looking back at previous row: {slice} = {is_previous}");
+        }
+        let slice = &current.unwrap()[pos .. len];
+        println!("cutting string {} at {pos}, {len}", current.unwrap());
+        is_current = SYMBLE_REGEX.is_match(slice.replace(".", " ").as_str());
+        println!("Looking at current row: {slice} {is_current}");
+
+        if forward.is_some() {
+            let slice = &forward.unwrap()[pos .. len];
+            println!("cutting string {} at {pos}, {len}", forward.unwrap());
+            is_future =  SYMBLE_REGEX.is_match(slice.replace(".", " ").as_str());
+            println!("Looking at forward row: {slice} {is_future}");
+        }
+        if is_previous || is_current || is_future {
+            total += part.parse::<u32>().unwrap();
+        }
+        let is_part = is_previous || is_current || is_future;
+        println!("part: {part} processed: {is_part} total: {total}");
+    }
+    
+    total
+}
+
+pub fn part_two(_input: &str) -> Option<u32> {
     None
 }
 
@@ -15,7 +81,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4361));
     }
 
     #[test]
